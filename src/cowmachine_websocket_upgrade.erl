a0047d604c650d4d6e77da46037742092804b495
Marc Worrell <marc@worrell.nl>
2016-09-01 09:39:39 +0200

    Remove patched cowboy websocket code, reply can be done on websocket controller using the normal cowboy_websocket.

--- src/cowmachine_websocket_upgrade.erl
+++ src/cowmachine_websocket_upgrade.erl
@@ -5,9 +5,8 @@
     ]).
 
 %% @doc Upgrade the request to a websocket request
-%% @todo Cleanup the context req, remove all unneeded data (maybe the whole req?)
 -spec upgrade(atom(), cowmachine_req:context()) -> {ok, cowboy_req:req(), cowboy_middleware:env()}.
 upgrade(Handler, Context) ->
     Req = cowmachine_req:req(Context),
     Env = #{},
-    cowmachine_websocket:upgrade(Req, Env, Handler, Context, infinity, run).
+    cowboy_websocket:upgrade(Req, Env, Handler, Context, infinity, run).
