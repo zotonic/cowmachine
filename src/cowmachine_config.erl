333c4981abc637ddb399b7182d99177c50c7c16d
Maas-Maarten Zeeman <mmzeeman@xs4all.nl>
2026-06-14 13:13:54 +0200

    Get the websockets options via cowmachine metadata instead of the application config

--- src/cowmachine_config.erl
+++ src/cowmachine_config.erl
@@ -7,7 +7,6 @@
 -module(cowmachine_config).
 
 -export([
-    ws_opts/0,
     env/2
 ]).
 
@@ -16,12 +15,4 @@ env(Key, Default) ->
         {ok, V} -> V;
         _ -> Default
     end.
-
-% Reasonably safe websocket config.
-ws_opts() ->
-    #{
-      idle_timeout => env(ws_idle_timeout, 120000), % For slow clients while also preventing slow client DoS
-      max_frame_size => env(ws_max_frame_size, 524288), % 512kb
-      compress => env(ws_compress, true)
-     }.
 
--- src/cowmachine_websocket_upgrade.erl
+++ src/cowmachine_websocket_upgrade.erl
@@ -13,7 +13,7 @@
 upgrade(Handler, Context) ->
     Req = cowmachine_req:req(Context),
     Env = cowmachine_req:env(Context),
-    Opts = cowmachine_config:ws_opts(),
+    Opts = ws_opts(Context),
 
     % Ensure the handler module is loaded. Cowboy uses erlang:function_exported/3 to
     % check if optional callbacks are available. When the handler module is not loaded
@@ -22,3 +22,20 @@ upgrade(Handler, Context) ->
     {module, Handler} = code:ensure_loaded(Handler),
 
     cowboy_websocket:upgrade(Req, Env, Handler, Context, Opts).
+
+% Reasonably safe websocket config.
+ws_opts(Context) ->
+    case cowmachine_req:get_metadata(ws_opts, Context) of
+        M when is_map(M) ->
+            maps:merge(ws_defaults, M);
+        _ ->
+            ws_defaults()
+    end.
+
+ws_defaults() ->
+    #{
+      idle_timeout => 120000, % For slow clients while also preventing slow client DoS
+      max_frame_size => 524288, % 512kb
+      compress => true
+     }.
+
